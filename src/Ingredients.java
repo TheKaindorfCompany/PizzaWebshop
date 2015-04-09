919aa1b162bd5eacd2b7798c7f0c8788e9bf056e
goevea11 <goevea11@htlkaindorf.at>
2015-04-09 12:16:47 +0200

    Ingredient Class

--- src/Ingredients.java
+++ src/Ingredients.java
@@ -2,4 +2,48 @@
  * Created by Veronika on 09.04.2015.
  */
 public class Ingredients {
+    //Name, Wieviel Stück, Link zum Bild, Preis pro Stück
+    private String name;
+    private String stk;
+    private String picture;
+    private float price;
+
+    public Ingredients(String picture, String name, float price, String stk) {
+        this.picture = picture;
+        this.name = name;
+        this.price = price;
+        this.stk = stk;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getStk() {
+        return stk;
+    }
+
+    public void setStk(String stk) {
+        this.stk = stk;
+    }
 }
