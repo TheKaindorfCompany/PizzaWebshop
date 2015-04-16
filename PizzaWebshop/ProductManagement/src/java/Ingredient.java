a343bc04c1f09a12ce41bd936dfa279e908a75a8
goevea11 <goevea11@htlkaindorf.at>
2015-04-16 11:52:02 +0200

    Ingredient class

---
+++ PizzaWebshop/ProductManagement/src/java/Ingredient.java
@@ -0,0 +1,53 @@
+package java;
+
+/**
+ * Created by Veronika on 16.04.2015.
+ */
+
+
+public class Ingredient {
+   // Name, Wieviel Stück, Link zum Bild, Preis pro Stück
+    private String name;
+    private String stk;
+    private String picture;
+    private float price;
+
+    public Ingredient(String picture, String name, float price, String stk) {
+        this.picture = picture;
+        this.name = name;
+        this.price = price;
+        this.stk = stk;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getStk() {
+        return stk;
+    }
+
+    public void setStk(String stk) {
+        this.stk = stk;
+    }
+}
