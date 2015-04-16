117a429a7b3ab83daa45a3b24b2fab2399e160f4
rotdoa10 <rotdoa10@htlkaindorf.at>
2015-04-16 11:12:20 +0200

    Class Member:
    Von Person abgeleitet
    Emali und Passwort hinzugefügt.
    
    Dominik

--- Customer Management/src/main/java/beans/Member.java
+++ Customer Management/src/main/java/beans/Member.java
@@ -4,4 +4,12 @@ package beans;
  * Created by Dominik on 16.04.2015.
  */
 public class Member extends Person {
+    String EMail;
+    String password;
+
+    public Member(String vorname, String nachname, Adresse adresse, String EMail, String password) {
+        super(vorname, nachname, adresse);
+        this.EMail = EMail;
+        this.password = password;
+    }
 }
