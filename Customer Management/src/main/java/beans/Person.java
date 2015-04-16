1c50024d11a9743c1f84ada299d4558565dafbe9
koldaa10 <koldaa10@htlkaindorf.at>
2015-04-16 11:11:10 +0200

    Class Person: created getter and setter methods
    David

--- Customer Management/src/main/java/beans/Person.java
+++ Customer Management/src/main/java/beans/Person.java
@@ -14,4 +14,28 @@ public class Person
         this.nachname = nachname;
         this.adresse = adresse;
     }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
 }
