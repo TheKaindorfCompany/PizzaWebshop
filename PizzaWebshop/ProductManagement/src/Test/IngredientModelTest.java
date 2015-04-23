5f2bd4a2e271ec08818476954440776a44b1d4c1
goevea11 <goevea11@htlkaindorf.at>
2015-04-23 11:35:09 +0200

    IngredientModelTest

---
+++ PizzaWebshop/ProductManagement/src/Test/IngredientModelTest.java
@@ -0,0 +1,13 @@
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Veronika on 23.04.2015.
+ */
+public class IngredientModelTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+}
