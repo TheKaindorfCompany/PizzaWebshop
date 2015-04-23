e514a22d661a3f1371565b402da302601d7830b2
korju11 <korjuc11@htlkaindorf.at>
2015-04-23 11:17:40 +0200

    ingredientModel added

---
+++ PizzaWebshop/ProductManagement/src/java/IngredientModel.java
@@ -0,0 +1,44 @@
+package java;
+import java.util.LinkedList;
+/**
+ * Created by Julian on 23.04.2015.
+ */
+public class IngredientModel {
+
+    private LinkedList<Ingredient> ingredientsList = new LinkedList<Ingredient>();
+
+    public IngredientModel(){
+
+    }
+
+
+    public void addIngredient(String picture,String name, String stk, float price)
+    {
+        Ingredient ingredient=new Ingredient(picture,name,price,stk);
+
+        if(!ingredientsList.contains(ingredient))
+        {
+            ingredientsList.add(ingredient);
+        }
+
+    }
+
+    public void deleteIngredient(Ingredient ingredient) {
+        ingredientsList.remove(ingredient);
+    }
+
+    public LinkedList<Ingredient> search(String searchString)
+    {
+        LinkedList<Ingredient> searchList = new LinkedList<Ingredient>();
+        for(Ingredient ingredient : ingredientsList)
+        {
+            String ingredientString = ingredient.getName();
+
+            if(ingredientString.contains(searchString))
+            {
+                searchList.add(ingredient);
+            }
+        }
+        return searchList;
+    }
+}
