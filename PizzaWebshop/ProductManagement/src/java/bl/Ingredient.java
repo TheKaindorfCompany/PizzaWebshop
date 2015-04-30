0851c638ec558f77bd0cb45010b3b4eeabc4c4e8
samdoa11 <samdoa11@htl-kaindorf.ac.at>
2015-04-30 11:54:11 +0200

    Struktur aufgeräumt ;P

---
+++ PizzaWebshop/ProductManagement/src/java/bl/Ingredient.java
@@ -0,0 +1,59 @@
+package bl;
+
+/**
+ * Created by Veronika on 16.04.2015.
+ */
+
+
+public class Ingredient {
+   // Name, Wieviel Stück, Link zum Bild, Preis pro Stück
+    private String name;
+    private int stk;
+    private String picture;
+    private float price;
+
+    public Ingredient(String picture, String name, float price, int stk) {
+        this.picture = picture;
+        this.name = name;
+        this.price = price;
+        this.stk = stk;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getStk() {
+        return stk;
+    }
+
+    public void setStk(int stk) {
+        this.stk = stk;
+    }
+
+    @Override
+    public String toString() {
+        return "<tr><td>" + this.name + "</td><td>" + this.price + "</td><td>"
+                + this.stk + "</td></tr>";
+    }
+}
---
+++ PizzaWebshop/ProductManagement/src/java/servlet/IngredientServlet.java
@@ -0,0 +1,16 @@
+package servlet;
+
+import java.io.IOException;
+
+/**
+ * Created by dominik on 23.04.2015.
+ */
+public class IngredientServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
