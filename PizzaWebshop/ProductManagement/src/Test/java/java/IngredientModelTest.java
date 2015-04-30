db78cf7f7960c7962f83d32617cdefd6606922c7
samdoa11 <samdoa11@htl-kaindorf.ac.at>
2015-04-30 11:23:44 +0200

    test

---
+++ PizzaWebshop/ProductManagement/src/Test/java/java/IngredientModelTest.java
@@ -0,0 +1,19 @@
+package java;
+
+import org.junit.Test;
+
+/**
+ * Created by Veronika on 23.04.2015.
+ */
+public class IngredientModelTest {
+
+    @Test
+    public void testGetInstance() throws Exception {
+
+    }
+
+    @Test
+    public void testGetIngredients() throws Exception {
+
+    }
+}
