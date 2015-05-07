315b50eeffe3e8d2a82c316087bd79a16afa1a13
korjuc11 <korjuc11@htlkaindorf.at>
2015-05-07 12:23:25 +0200

    productlist edited

--- PizzaWebshop/ProductManagement/src/java/bl/IngredientModel.java
+++ PizzaWebshop/ProductManagement/src/java/bl/IngredientModel.java
@@ -8,29 +8,23 @@ import java.util.LinkedList;
  * Created by Julian on 07.05.2015.
  */
 public class IngredientModel {
-
-    private String restName="";
     private DB_Access dba;
     private LinkedList<Ingredient> ingredientList;
     public IngredientModel(String restName)
     {
-        this.restName=restName;
         try {
-             dba = DB_Access.getInstance();
+            dba = DB_Access.getInstance();
             this.ingredientList = new LinkedList<Ingredient>();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
     }
 
 
-    public void getIngredientList() throws Exception {
+    public LinkedList<Ingredient> getIngredientList(String restName) throws Exception {
 
-        ingredientList = dba.getIngredients(restName);
-
-
-
-
+        return dba.getIngredients(restName);
 
     }
 
