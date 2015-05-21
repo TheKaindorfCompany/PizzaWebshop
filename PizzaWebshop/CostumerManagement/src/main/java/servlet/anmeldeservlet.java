cc795ba6141fac5609278f4a263cdd34d96323b3
koldaa10 <koldaa10@htlkaindorf.at>
2015-05-21 11:29:18 +0200

    anmeldeservlet created and web.xml + pom.xml updated
    David

---
+++ PizzaWebshop/CostumerManagement/src/main/java/servlet/anmeldeservlet.java
@@ -0,0 +1,16 @@
+package servlet;
+
+import java.io.IOException;
+
+/**
+ * Created by David on 21.05.2015.
+ */
+public class anmeldeservlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
