59c437d3d0b94544c0a953d08c3ab1a2a0070cea
Jürgen <lutjuc11@htlkaindorf.at>
2015-05-21 12:13:50 +0200

    DB_Update

--- PizzaWebshop/PizzaShopDB/script.sql
+++ PizzaWebshop/PizzaShopDB/script.sql
@@ -1,17 +1,38 @@
 ALTER TABLE ingredients_in_product
 DROP CONSTRAINT ing_pro_PK;
-ALTER TABLE products
-DROP CONSTRAINT pro_PK;
+ALTER TABLE ingredients_in_product
+DROP CONSTRAINT inp_pro_FK;
+ALTER TABLE ingredients_in_product
+DROP CONSTRAINT inp_ing_FK;
+
 ALTER TABLE ingredients
 DROP CONSTRAINT ing_PK;
+ALTER TABLE ingredients
+DROP CONSTRAINT ing_res_FK;
+
+ALTER TABLE products
+DROP CONSTRAINT pro_PK;
+ALTER TABLE products
+DROP CONSTRAINT pro_res_FK;
+
+ALTER TABLE restaurant
+DROP CONSTRAINT res_adr_PK;
+ALTER TABLE restaurant
+DROP CONSTRAINT res_adr_FK;
+
+ALTER TABLE address
+DROP CONSTRAINT adr_PK;
 
 DROP TABLE ingredients_in_product;
 DROP TABLE products;
 DROP TABLE ingredients;
+DROP TABLE restaurant;
+DROP TABLE address;
 
 CREATE TABLE products
 (
   product_id NUMBER(10, 0),
+  restaurant_id NUMBER(10, 0),
   name VARCHAR2(50) NOT NULL,
   price NUMBER(5, 2) NOT NULL,
   picture VARCHAR(50)
@@ -20,6 +41,7 @@ CREATE TABLE products
 CREATE TABLE ingredients
 (
   ingredient_id NUMBER(10, 0),
+  restaurant_id NUMBER(10, 0),
   name VARCHAR2(50) NOT NULL,
   price NUMBER(5, 2) NOT NULL,
   amount NUMBER(10, 0) NOT NULL,
@@ -29,12 +51,50 @@ CREATE TABLE ingredients
 CREATE TABLE ingredients_in_product
 (
   product_id NUMBER(10, 0),
-  ingredient_id NUMBER(10, 0)
+  ingredient_id NUMBER(10, 0),
+  restaurant_id NUMBER(10, 0)
 );
 
+CREATE TABLE address
+(
+  address_id NUMBER(10, 0),
+  street VARCHAR2(50) NOT NULL,
+  zip VARCHAR2(10) NOT NULL,
+  town VARCHAR2(50) NOT NULL,
+  xChord NUMBER(10,10) NOT NULL,
+  yChord NUMBER(10,10) NOT NULL
+);
+
+CREATE TABLE restaurant
+(
+  restaurant_id NUMBER(10, 0),
+  address_id NUMBER(10, 0) NOT NULL,
+  name VARCHAR2(50) NOT NULL,
+  description VARCHAR2(50) NOT NULL,
+  picture VARCHAR(50)
+);
+
+ALTER TABLE address
+ADD CONSTRAINT adr_PK PRIMARY KEY (address_id);
+
+ALTER TABLE restaurant
+ADD CONSTRAINT res_adr_FK FOREIGN KEY (address_id) REFERENCES address(address_id);
+ALTER TABLE restaurant
+ADD CONSTRAINT res_adr_PK PRIMARY KEY (restaurant_id);
+
 ALTER TABLE products
-ADD CONSTRAINT pro_PK PRIMARY KEY (product_id);
+ADD CONSTRAINT pro_res_FK FOREIGN KEY (restaurant_id) REFERENCES restaurant(restaurant_id);
+ALTER TABLE products
+ADD CONSTRAINT pro_PK PRIMARY KEY (product_id, restaurant_id);
+
 ALTER TABLE ingredients
-ADD CONSTRAINT ing_PK PRIMARY KEY (ingredient_id);
+ADD CONSTRAINT ing_res_FK FOREIGN KEY (restaurant_id) REFERENCES restaurant(restaurant_id);
+ALTER TABLE ingredients
+ADD CONSTRAINT ing_PK PRIMARY KEY (ingredient_id, restaurant_id);
+
 ALTER TABLE ingredients_in_product
-ADD CONSTRAINT ing_pro_PK PRIMARY KEY (product_id, ingredient_id);
+ADD CONSTRAINT inp_pro_FK FOREIGN KEY (product_id, restaurant_id) REFERENCES products(product_id, restaurant_id);
+ALTER TABLE ingredients_in_product
+ADD CONSTRAINT inp_ing_FK FOREIGN KEY (ingredient_id, restaurant_id) REFERENCES ingredients(ingredient_id, restaurant_id);
+ALTER TABLE ingredients_in_product
+ADD CONSTRAINT ing_pro_PK PRIMARY KEY (product_id, ingredient_id, restaurant_id);
