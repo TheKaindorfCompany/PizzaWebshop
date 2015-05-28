df491f892ed3686034a292827e26ba323cf2a4fe
samdoa11 <samdoa11@htl-kaindorf.ac.at>
2015-05-28 11:12:16 +0200

    fix test

---
+++ PizzaWebshop/ProductManagement/src/Test/java/test/ProductListTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.junit.Test;
+
+/**
+ * Created by Veronika on 23.04.2015.
+ */
+public class ProductListTest {
+
+    @Test
+    public void testGetInstance() throws Exception {
+
+    }
+
+    @Test
+    public void testGetIngredients() throws Exception {
+
+    }
+}
