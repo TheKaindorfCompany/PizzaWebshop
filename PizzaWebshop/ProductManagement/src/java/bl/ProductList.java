cd58d6b0f36e3647a7522f95982518f550be0ea2
goevea11 <goevea11@htlkaindorf.at>
2015-06-11 12:07:37 +0200

    Code Dokumentation

--- PizzaWebshop/ProductManagement/src/java/bl/ProductList.java
+++ PizzaWebshop/ProductManagement/src/java/bl/ProductList.java
@@ -1,7 +1,8 @@
 package bl;
 import java.util.LinkedList;
 /**
- * Created by Julian on 23.04.2015.
+ * Product Management
+ * Verwaltungsklasse für die Produktliste
  */
 public class ProductList {
 
@@ -13,6 +14,7 @@ public class ProductList {
 
     public Product getCurrentProduct(int index) throws IllegalArgumentException,IndexOutOfBoundsException
     {
+        // Gibt das Aktuelle Produkt zurück anhand des Indexes
         if (index < 0) throw new IllegalArgumentException("Index can not be less than zero!");
         if (this.productList.size() <= index) throw new IndexOutOfBoundsException("Index is fewer than max size of list!");
         return productList.get(index);
@@ -21,6 +23,7 @@ public class ProductList {
 
     public void addProduct(LinkedList<Ingredient> ingredientList, String name, String productId, String restName)
     {
+        // Fügt ein neues Produkt hinzu
         Product product = new Product(ingredientList, name, productId, restName);
 
         if(!productList.contains(product))
@@ -31,6 +34,7 @@ public class ProductList {
     }
 
     public void addProductObject(Product p) throws IllegalArgumentException {
+        // Fügt ein neues ProduktObjekt in die Liste
         if (p == null) {
             throw new IllegalArgumentException("Product is null!");
         }
@@ -42,12 +46,14 @@ public class ProductList {
     }
 
     public void deleteProduct(Product product) throws IllegalArgumentException{
+        // Löscht ein Produkt aus der Liste
         if (product == null) throw new IllegalArgumentException("Error! Product to delete is null");
         productList.remove(product);
     }
 
     public LinkedList<Product> getProductsFromRestaurant(String restaurantName) throws IllegalArgumentException
     {
+        // Vom Restaurant alle Produkte zurückgeben
         if (restaurantName == "") throw new IllegalArgumentException("No restaurant name!");
 
         LinkedList<Product> searchList = new LinkedList<Product>();
