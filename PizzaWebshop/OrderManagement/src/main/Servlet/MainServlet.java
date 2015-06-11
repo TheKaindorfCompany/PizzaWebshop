586dcc46688d01778c60104cbc8ebedb69622e08
haryvc11 <haryvc11@htlkaindorf.at>
2015-06-11 11:52:42 +0200

    Servlet

---
+++ PizzaWebshop/OrderManagement/src/main/Servlet/MainServlet.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+/**
+ * Created by Yvonne on 11.06.2015.
+ */
+public class MainServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
