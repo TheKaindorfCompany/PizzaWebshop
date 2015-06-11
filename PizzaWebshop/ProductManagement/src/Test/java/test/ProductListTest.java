ce0b378662d7a019e722a54c4e6a7abd3c9d279b
samdoa11 <samdoa11@htl-kaindorf.ac.at>
2015-06-11 12:19:01 +0200

    comments

--- PizzaWebshop/ProductManagement/src/Test/java/test/ProductListTest.java
+++ PizzaWebshop/ProductManagement/src/Test/java/test/ProductListTest.java
@@ -12,30 +12,36 @@ import java.util.LinkedList;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 /**
- * Created by Veronika on 23.04.2015.
+ * ProductManagment
+ * Tests for the ProductList class
  */
 public class ProductListTest {
     private ProductList m_ProdList;
 
     @Before
     public void init() {
+        //performs basic initiaslisations
         this.m_ProdList = new ProductList();
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void testIfAddProductNullThrowsError() {
+        //Tests if Error is returned when I would like a product which is null
         Product p = null;
         this.m_ProdList.addProductObject(p);
     }
 
     @Test
     public void testIfYouCanAddProducts() {
+        //Tests if you can add products
         Product p = this.createTestProduct();
         this.m_ProdList.addProductObject(p);
+        Assert.assertThat(this.m_ProdList.getCurrentProduct(0), equalTo(p));
     }
 
     @Test
     public void testCountElements() {
+        //Tests if count Elements works
         Product p1 = this.createTestProduct();
         Product p2 = this.createTestProduct();
 
@@ -47,6 +53,7 @@ public class ProductListTest {
 
     @Test
     public void testDeleteProduct() {
+        //Tests if products are deleted
         Product p1 = this.createTestProduct();
         Product p2 = this.createTestProduct();
 
@@ -58,6 +65,7 @@ public class ProductListTest {
 
     @Test (expected = IllegalArgumentException.class)
     public void testIfDeleteProductThrowsErrorIfProductIsNull() {
+        //tests if delete product returns error when I wanted to delete a product which is null
         Product p1 = this.createTestProduct();
         Product p2 = this.createTestProduct();
 
@@ -68,6 +76,7 @@ public class ProductListTest {
 
     @Test
     public void testIfRightProductIsReturned() {
+        //tests if the correct product is returned
         Product p1 = this.createTestProduct();
         Product p2 = this.createTestProduct();
 
@@ -78,6 +87,7 @@ public class ProductListTest {
 
     @Test (expected = IndexOutOfBoundsException.class)
     public void testIfIndexOutOfBoundsExceptionIsThrown() {
+        //Tests if index out of bounds exception is thrown
         Product p1 = this.createTestProduct();
         Product p2 = this.createTestProduct();
 
@@ -88,6 +98,7 @@ public class ProductListTest {
 
     @Test (expected = IllegalArgumentException.class)
     public void testIfIllegalArgumentExceptionIsThrownIfIWantProductMinus1() {
+        //test if illegal argument exception is thrown
         Product p1 = this.createTestProduct();
         Product p2 = this.createTestProduct();
 
@@ -98,6 +109,7 @@ public class ProductListTest {
 
     @Test
     public void testIfGetProductsOfRestaurantReturnsTheRightProducts() {
+        //tests if the get products from restaurant
         Product p1 = this.createTestProduct();
         Product p2 = this.createTestProduct();
 
@@ -110,6 +122,7 @@ public class ProductListTest {
 
     @Test (expected = IllegalArgumentException.class)
     public void testIfGetProductOfRestaurantReturnsError() {
+        //tests if error is thrown
         Product p1 = this.createTestProduct();
         Product p2 = this.createTestProduct();
 
@@ -119,6 +132,7 @@ public class ProductListTest {
         LinkedList<Product> products = this.m_ProdList.getProductsFromRestaurant("");
     }
 
+    //Creates test data for the product
     private Product createTestProduct() {
         Ingredient testIng = new Ingredient("Cola",1);
         Ingredient testIng1 = new Ingredient("Eiswürfel",0.2f);
